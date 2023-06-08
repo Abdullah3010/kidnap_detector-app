26a68c117f04652fb9934933f6d771758b2d025b
Mac <mac@Macs-MacBook-Pro.local>
2023-06-08 11:35:33 +0300

    save

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Applications/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Applications/Flutter_Project/kidnap_app/kidnap_detector_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Applications/Flutter_Project/kidnap_app/kidnap_detector_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.101"
+export "FLUTTER_BUILD_NUMBER=101"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85MGZhM2FlMjhmZTZkZGFlZTFhZjJjMTIwZjAxZTUwMjAxYzE0MDFiLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Applications/Flutter_Project/kidnap_app/kidnap_detector_app/.dart_tool/package_config.json"
